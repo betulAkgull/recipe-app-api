92117c1ebfa9bc3b1cade4085edd2b5654203aa8
Betül Akgül <76072632+betulAkgull@users.noreply.github.com>
2024-09-02 18:42:01 +0300

    recipe tags api crud ops added

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -11,6 +11,10 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 
+def create_user(email='user@example.com', password='testpass123'):
+    return get_user_model().objects.create_user(email, password)
+
+
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successfull(self):
@@ -67,3 +71,11 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        "test creating a tag is successful"
+
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag')
+
+        self.assertEqual(str(tag), tag.name)
